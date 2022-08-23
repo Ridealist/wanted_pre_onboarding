90243f939f142c587819a51cdf7248d33679cff0
Ridealist <cjsn92@gmail.com>
2022-08-23 23:53:15 +0900

    fix/apply: Change views' architects from APIView to ViewSets

--- recruit/apply/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ApplyListCreateView
-
-app_name = "apply"
-urlpatterns = [
-    path("", ApplyListCreateView.as_view(), name="apply-create"),
-]
--- recruit/apply/views.py
+++ recruit/apply/views.py
@@ -1,9 +1,10 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import CreateModelMixin
 
 from .models import Apply
 from .serializers import ApplySerializer
 
 
-class ApplyListCreateView(ListCreateAPIView):
+class ApplyViewSet(GenericViewSet, CreateModelMixin):
     queryset = Apply.objects.all()
     serializer_class = ApplySerializer
