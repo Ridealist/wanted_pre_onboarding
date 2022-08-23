b0b0855d6824a9091eba0e10c631bd29bedec6bb
Ridealist <cjsn92@gmail.com>
2022-08-23 23:56:50 +0900

    feat/user: Explicitly Define User model.

--- recruit/user/models.py
+++ recruit/user/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = None
+    last_name = None
+
+    class Meta(AbstractUser.Meta):
+        swappable = "AUTH_USER_MODEL"
+        abstract = False
